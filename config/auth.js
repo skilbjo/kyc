9f1a96cd69aacc5787389f62e68d4484d7dc77fe
John Skilbeck <john.skilbeck@gmail.com>
2014-01-20 19:13:41 -0800

    fb finished, working on twitter

--- config/auth.js
+++ config/auth.js
@@ -1,1 +1,24 @@
-// auth.js
+// config/auth.js
+
+// expose our config directly to our application using module.exports
+module.exports = {
+
+	'facebookAuth' : {
+		'clientID' 		: '188602714683480', // your App ID
+		'clientSecret' 	: '5b9bd9c90937c2c615e5bf8074ec79de', // your App Secret
+		'callbackURL' 	: 'http://localhost:8080/auth/facebook/callback'
+	},
+
+	'twitterAuth' : {
+		'consumerKey' 		: 'N08lR2r8prSHqx6RLb3Qzw',
+		'consumerSecret' 	: 'ZM34tiImp5Uth2zervfTqKYV4y2ebAWue2Kzp8N2PU',
+		'callbackURL' 		: 'http://localhost:8080/auth/twitter/callback'
+	},
+
+	'googleAuth' : {
+		'clientID' 		: 'your-secret-clientID-here',
+		'clientSecret' 	: 'your-client-secret-here',
+		'callbackURL' 	: 'http://localhost:8080/auth/google/callback'
+	}
+
+};
