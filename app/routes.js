69ec41b16e063ffd8bde2577b0de94e5067d4583
John Skilbeck <john.skilbeck@gmail.com>
2014-01-22 21:57:29 -0800

    starting to link/unlink accounts. tbd: changing db to local mongodb

--- app/routes.js
+++ app/routes.js
@@ -53,31 +53,59 @@ module.exports = function(app, passport) {
 		});
 	});
 
-	// =====================================
-	// FACEBOOK ROUTES =====================
-	// =====================================
-	// route for facebook authentication and login
-	app.get('/auth/facebook', passport.authenticate('facebook', { scope : 'email' }));
+	// =========================================================================
+    // FACEBOOK ================================================================
+    // =========================================================================
+    passport.use(new FacebookStrategy({
 
-	// handle the callback after facebook has authenticated the user
-	app.get('/auth/facebook/callback',
-		passport.authenticate('facebook', {
-			successRedirect : '/profile',
-			failureRedirect : '/'
-		}));
+        // pull in our app id and secret from our auth.js file
+        clientID        : configAuth.facebookAuth.clientID,
+        clientSecret    : configAuth.facebookAuth.clientSecret,
+        callbackURL     : configAuth.facebookAuth.callbackURL
 
-	// =====================================
-	// TWITTER ROUTES ======================
-	// =====================================
-	// route for twitter authentication and login
-	app.get('/auth/twitter', passport.authenticate('twitter'));
+    },
 
-	// handle the callback after twitter has authenticated the user
-	app.get('/auth/twitter/callback',
-		passport.authenticate('twitter', {
-			successRedirect : '/profile',
-			failureRedirect : '/'
-		}));
+    // facebook will send back the token and profile
+    function(token, refreshToken, profile, done) {
+
+        // asynchronous
+        process.nextTick(function() {
+
+            // find the user in the database based on their facebook id
+            User.findOne({ 'facebook.id' : profile.id }, function(err, user) {
+
+                // if there is an error, stop everything and return that
+                // ie an error connecting to the database
+                if (err)
+                    return done(err);
+
+                // if the user is found, then log them in
+                if (user) {
+                    return done(null, user); // user found, return that user
+                } else {
+                    // if there is no user found with that facebook id, create them
+                    var newUser            = new User();
+
+                    // set all of the facebook information in our user model
+                    newUser.facebook.id    = profile.id; // set the users facebook id                   
+                    newUser.facebook.token = token; // we will save the token that facebook provides to the user                    
+                    newUser.facebook.name  = profile.name.givenName + ' ' + profile.name.familyName; // look at the passport user profile to see how names are returned
+                    newUser.facebook.email = profile.emails[0].value; // facebook can return multiple emails so we'll take the first
+
+                    // save our user to the database
+                    newUser.save(function(err) {
+                        if (err)
+                            throw err;
+
+                        // if successful, return the new user
+                        return done(null, newUser);
+                    });
+                }
+
+            });
+        });
+
+    }));
 
 	// =====================================
 	// GOOGLE ROUTES =======================
@@ -102,6 +130,51 @@ module.exports = function(app, passport) {
 		res.redirect('/');
 	});
 };
+
+
+	// =============================================================================
+	// UNLINK ACCOUNTS =============================================================
+	// =============================================================================
+	// used to unlink accounts. for social accounts, just remove the token
+	// for local account, remove email and password
+	// user account will stay active in case they want to reconnect in the future
+
+	    // local -----------------------------------
+	    app.get('/unlink/local', function(req, res) {
+	        var user            = req.user;
+	        user.local.email    = undefined;
+	        user.local.password = undefined;
+	        user.save(function(err) {
+	            res.redirect('/profile');
+	        });
+	    });
+
+	    // facebook -------------------------------
+	    app.get('/unlink/facebook', function(req, res) {
+	        var user            = req.user;
+	        user.facebook.token = undefined;
+	        user.save(function(err) {
+	            res.redirect('/profile');
+	        });
+	    });
+
+	    // twitter --------------------------------
+	    app.get('/unlink/twitter', function(req, res) {
+	        var user           = req.user;
+	        user.twitter.token = undefined;
+	        user.save(function(err) {
+	           res.redirect('/profile');
+	        });
+	    });
+
+	    // google ---------------------------------
+	    app.get('/unlink/google', function(req, res) {
+	        var user          = req.user;
+	        user.google.token = undefined;
+	        user.save(function(err) {
+	           res.redirect('/profile');
+	        });
+	    });
 
 // route middleware to make sure a user is logged in
 function isLoggedIn(req, res, next) {
