b9c2f33dda44b525d91bd8c659fbd86f54a1bf3e
John Skilbeck <john.skilbeck@gmail.com>
2014-02-20 13:39:30 -0800

    registered handlebars helpers

--- server.js
+++ server.js
@@ -2,13 +2,17 @@
 
 // set up ======================================================================
 // get all the tools we need
-var express  = require('express');
-var app      = express();
-var port     = process.env.PORT || 8080;
-var mongoose = require('mongoose');
-var passport = require('passport');
-var flash 	 = require('connect-flash');
-var configDB = require('./config/database.js');
+
+// change the below to this style
+var 
+	express  = require('express'),
+	app      = express(),
+	port     = process.env.PORT || 8080,
+	passport = require('passport'),
+	flash 	 = require('connect-flash'),
+	mongoose = require('mongoose'),
+	hbs  	 = require('hbs'),
+	configDB = require('./config/database.js');
 
 // configuration ===============================================================
 mongoose.connect(configDB.url); // connect to our database
@@ -21,14 +25,62 @@ app.configure(function() {
 	app.use(express.urlencoded()); // get information from html forms
 	app.use(express.json());
 
+	// handlebars engine for templating :-}
 	app.set('view engine', 'hbs');
 	app.set('views', __dirname + '/app/views');
 
 	// required for passport
-	app.use(express.session({ secret: 'ilovescotch' })); // session secret
+	app.use(express.session({ secret: 'passportapp' })); // session secret
 	app.use(passport.initialize());
 	app.use(passport.session()); // persistent login sessions
 	app.use(flash()); // use connect-flash for flash messages stored in session
+
+});
+
+// handlebars stuff ===========================================================
+
+// hbs.handlebars is the handlebars module
+hbs.handlebars === require('handlebars');
+
+// register handlebars helpers =================================================
+
+hbs.registerHelper('compare', function (lvalue, operator, rvalue, options) {
+
+    var operators, result;
+    
+    if (arguments.length < 3) {
+        throw new Error("Handlerbars Helper 'compare' needs 2 parameters");
+    }
+    
+    if (options === undefined) {
+        options = rvalue;
+        rvalue = operator;
+        operator = "===";
+    }
+    
+    operators = {
+        '==': function (l, r) { return l == r; },
+        '===': function (l, r) { return l === r; },
+        '!=': function (l, r) { return l != r; },
+        '!==': function (l, r) { return l !== r; },
+        '<': function (l, r) { return l < r; },
+        '>': function (l, r) { return l > r; },
+        '<=': function (l, r) { return l <= r; },
+        '>=': function (l, r) { return l >= r; },
+        'typeof': function (l, r) { return typeof l == r; }
+    };
+    
+    if (!operators[operator]) {
+        throw new Error("Handlerbars Helper 'compare' doesn't know the operator " + operator);
+    }
+    
+    result = operators[operator](lvalue, rvalue);
+    
+    if (result) {
+        return options.fn(this);
+    } else {
+        return options.inverse(this);
+    }
 
 });
 
